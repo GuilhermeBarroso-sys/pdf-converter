1471ea7c3b1c5bf7e80d9e3049ad95494d376aa3
GuilhermeBarroso-sys <guilhermebarrosodeoliveira@outlook.com>
2021-02-17 14:51:42 -0300

    writer

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const Reader = require("./Reader");
 const Processor = require("./Processor");
 const Table = require("./Table");
 const HtmlParser = require("./HtmlParser");
+const Writer = require("./Writer");
 var reader = new Reader();
-
-
+var writer = new Writer();
 async function main() {
     var data = await reader.read("./users.csv");
     var processorData = Processor.Process(data);
@@ -12,7 +12,7 @@ async function main() {
     
     var html = await HtmlParser.Parse(users);
     console.log(html);
-    
+    writer.Write(Date.now() + ".html",html);
 }
 
 main();
